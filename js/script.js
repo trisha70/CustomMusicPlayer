900cd4ae003d31a265839677d89634688792f7b5
Trisha Banerjee <149897636+trisha70@users.noreply.github.com>
2025-07-30 03:27:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ window.addEventListener("load", () => {
 function loadMusic(indexNumb) {
   musicName.innerText = allMusic[indexNumb - 1].name;
   musicArtist.innerText = allMusic[indexNumb - 1].artist;
-  musicImg.src = `Images/${allMusic[indexNumb - 1].img}.jpg`;
+  musicImg.src = `images/${allMusic[indexNumb - 1].img}.jpg`;
   mainAudio.src = `songs/${allMusic[indexNumb - 1].src}.mp3`;
 }
 //play music function
