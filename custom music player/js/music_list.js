e94a344bf958e75d55864b018c4038efffbce3bf
Trisha Banerjee <149897636+trisha70@users.noreply.github.com>
2025-12-24 14:19:23 +0530

    Add files via upload

---
+++ custom music player/js/music_list.js
@@ -0,0 +1,32 @@
+let allMusic=[
+    {
+        name:"Shape of You",
+        artist:"Ed Sheeran",
+        img:"img1",
+        src:"img1"
+    },
+    {
+        name:"Perfect",
+        artist:"Ed Sheeran",
+        img:"img2",
+        src:"img2"
+    },
+    {
+        name:"Love me like you do",
+        artist:"Ellie Goulding",
+        img:"img3",
+        src:"img3"
+    },
+    {
+        name:"Cruel Summer",
+        artist:"Taylor Swift ",
+        img:"img4",
+        src:"img4"
+    },
+    {
+        name:"Lover",
+        artist:"Taylor Swift",
+        img:"img5",
+        src:"img5"
+    }
+];
